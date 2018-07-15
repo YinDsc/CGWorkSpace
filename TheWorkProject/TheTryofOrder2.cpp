cee207643e850a91b5bd8a81ae1cae66d55f1773
YinLoveDsc <YinLoveDsc@outlook.com>
2018-07-15 11:04:44 +0800

    CG's First push

---
+++ TheWorkProject/TheTryofOrder2.cpp
@@ -0,0 +1,14 @@
+//这是关于排序算法的第二次尝试。
+#include <iostream>
+using namespace std;
+int 
+int main(int argc, char const *argv[])
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b && a>c && b>c){
+        cout<<"The biggest number is"<<a<<endl;
+        
+    }
+    return 0;
+}
---
+++ TheWorkProject/TheTryofOrder3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int c;
+    cin>>c;
+    
+    //以上这些语句等价于int a，b，c；
+    while()
+    if()
+    return 0;
+}
